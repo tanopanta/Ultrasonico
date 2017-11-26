8861ab37adf99992bdce80bbfc7f2a394e820a43
tanopanta <rainstorm2525@gmail.com>
2017-11-26 19:45:53 +0900

    fftして振幅計算して表示まで

--- app/src/main/java/com/example/tattata/ultrasonico/MainActivity.java
+++ app/src/main/java/com/example/tattata/ultrasonico/MainActivity.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import org.jtransforms.fft.DoubleFFT_1D;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,30 +22,43 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     static final int SAMPLING_RATE = 44100;
+    static final int FFT_POINT = 4096;
     int bufSize;
     boolean isRecording = false;
     AudioRecord audioRecord;
     Thread thread;
     LineChart chart;
+    DoubleFFT_1D fft;
     short buf[];
+    double amp[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bufSize = AudioRecord.getMinBufferSize(SAMPLING_RATE,
                 AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        if(bufSize < FFT_POINT) {
+            bufSize = FFT_POINT;
+        }
 
         chart = findViewById(R.id.chart);
+        YAxis leftaxis = chart.getAxisLeft();
+        leftaxis.setAxisMaxValue(100000);
+        leftaxis.setAxisMinValue(-100000);
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
 
+        fft = new DoubleFFT_1D(FFT_POINT);
 
     }
-    public void display(short[] data) {
+    public void display(double[] data) {
         List<LineDataSet> dataSets = new ArrayList<>();
         ArrayList<String> xValues = new ArrayList<>();
         ArrayList<Entry> value = new ArrayList<>();
-        for(int i = 0; i < data.length; i++) {
-            xValues.add(String.valueOf(i));
-            value.add(new Entry(data[i], i));
+        for(int i = 0; i < data.length/2; i+= 10) {
+            xValues.add(String.valueOf((SAMPLING_RATE / (double) FFT_POINT) * (i / 2)));
+            double result = data[i];
+            value.add(new Entry((float)result, i / 10));
         }
         LineDataSet valueDataSet = new LineDataSet(value, "sample");
         dataSets.add(valueDataSet);
@@ -69,9 +84,22 @@ public class MainActivity extends AppCompatActivity {
                 buf = new short[bufSize];
                 while (isRecording) {
                     audioRecord.read(buf, 0, buf.length);
+
+                    double d[] = new double[FFT_POINT];
+                    for(int i = 0; i < FFT_POINT; i++) {
+                        d[i] = (double)buf[i];
+                    }
+                    fft.realForward(d);
+
+                    //実部と虚部が交互にある。そこから振幅を求める
+                    amp = new double[FFT_POINT / 2];
+                    for(int i = 0; i < FFT_POINT; i+=2) {
+                        amp[i / 2] = Math.abs(d[i]) + Math.abs(d[i + 1]);
+                    }
+
                     runOnUiThread(new Runnable() {
                         public void run() {
-                            display(buf);
+                            display(amp);
                         }
                     });
                 }
@@ -80,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
                 audioRecord.release();
             }
         });
-        //スレッドのスタート
         thread.start();
     }
     @Override

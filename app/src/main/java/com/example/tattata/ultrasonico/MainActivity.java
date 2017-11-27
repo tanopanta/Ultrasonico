9417374cd2f299907782afc9c8e9d69cab6c6d6f
tanopanta <rainstorm2525@gmail.com>
2017-11-28 00:45:35 +0900

    周波数との対応を修正

--- app/src/main/java/com/example/tattata/ultrasonico/MainActivity.java
+++ app/src/main/java/com/example/tattata/ultrasonico/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.tattata.ultrasonico;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.net.rtp.AudioCodec;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.YAxis;
@@ -16,7 +14,6 @@ import com.github.mikephil.charting.data.LineDataSet;
 import org.jtransforms.fft.DoubleFFT_1D;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     static final int SAMPLING_RATE = 44100;
     static final int FFT_POINT = 4096;
     static final double BASELINE = Math.pow(2, 15) * FFT_POINT * 2;//測定可能な最大振幅？ 16bit*FFT*2
+    static final int DISPLAY_INTERVAL = 1;
     int bufSize;
     boolean isRecording = false;
     AudioRecord audioRecord;
@@ -56,15 +54,21 @@ public class MainActivity extends AppCompatActivity {
         List<LineDataSet> dataSets = new ArrayList<>();
         ArrayList<String> xValues = new ArrayList<>();
         ArrayList<Entry> value = new ArrayList<>();
-        for(int i = 0; i < data.length/2; i+=10) {
-            xValues.add(String.valueOf((SAMPLING_RATE / (double) FFT_POINT) * (i / 2)));
-            double result = 20 * Math.log10(data[i] / BASELINE);
-            value.add(new Entry((float)result, i/10));
+        for(int i = 0; i < data.length/2; i+=DISPLAY_INTERVAL) {
+            xValues.add(String.valueOf(indexToFrequency(i)));
+            double result = amplitudeToDecibel(data[i]);
+            value.add(new Entry((float)result, i / DISPLAY_INTERVAL));
         }
         LineDataSet valueDataSet = new LineDataSet(value, "sample");
         dataSets.add(valueDataSet);
         chart.setData(new LineData(xValues, dataSets));
         chart.invalidate();
+    }
+    public double amplitudeToDecibel(double data) {
+        return  20 * Math.log10(data / BASELINE);
+    }
+    public int indexToFrequency(int index) {
+        return (int)((SAMPLING_RATE / (double) FFT_POINT) * index);
     }
 
     @Override
@@ -86,14 +90,14 @@ public class MainActivity extends AppCompatActivity {
                 while (isRecording) {
                     audioRecord.read(buf, 0, buf.length);
 
-                    double d[] = new double[FFT_POINT];
+                    double d[] = new double[FFT_POINT * 2];
                     for(int i = 0; i < FFT_POINT; i++) {
                         d[i] = (double)buf[i];
                     }
-                    fft.realForward(d);
+                    fft.realForwardFull(d);
 
                     //実部と虚部が交互にある。そこから振幅を求める
-                    amp = new double[FFT_POINT / 2];
+                    amp = new double[FFT_POINT];
                     for(int i = 0; i < FFT_POINT; i+=2) {
                         amp[i / 2] = Math.abs(d[i]) + Math.abs(d[i + 1]);
                     }
